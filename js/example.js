4b488f7873da2954169b02e195e8d876c252fe38
Gabr1elSwan <73124668+Gabr1elSwan@users.noreply.github.com>
2021-01-09 21:43:48 +0200

    добавил файлы с 5 домашки

---
+++ js/example.js
@@ -0,0 +1,15 @@
+function add(a, b, c) {
+    //  Пиши код ниже этой строки
+    console.log(`Сумма трех чисел равна`);
+    return a + b + c;
+    console.log()
+
+
+    //  Пиши код выше этой строки
+}
+
+add(2, 5, 8); // 15
+
+console.log(add(15, 27, 10));
+console.log(add(10, 20, 30));
+console.log(add(5, 10, 15));
